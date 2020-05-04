040fa6268150f74ec9bd0938ab600304afd7cce3
Steve Wang <ywang155@gmail.com>
2020-05-03 20:17:46 -0700

    improve path issues

--- scraper/pipelines.py
+++ scraper/pipelines.py
@@ -7,20 +7,22 @@
 
 from scrapy import Request
 from scrapy.pipelines.images import ImagesPipeline
+import random
 import os
 
 class UpdatedImagesPipeLine(ImagesPipeline):
   def get_media_requests(self, item, info):
-    return [Request(x, meta={'image_name': item['name']})
+    return [Request(x, meta={'restaurant_name': item['name']})
     	for x in item.get('image_urls', [])]
 
   def get_images(self, response, request, info):
     for path, image, buf, in super(UpdatedImagesPipeLine, self).get_images(response, request, info):
       path = self.change_filename(path, response)
+      print(path)
       yield path, image, buf
 
   def change_filename(self, key, response):
-    return '%s/image.jpg' % response.meta['image_name']
+    return '%s/%d.jpg' % (response.meta['restaurant_name'], random.getrandbits(128))
 
 
 class WriteTextPipeline:
